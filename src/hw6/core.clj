2c383e2e834ad0595cba0362d8c2a436205d95ef
Tim McCormack <cortex@brainonfire.net>
2011-04-07 16:30:23 -0400

    Parser without optional features.

--- src/hw6/core.clj
+++ src/hw6/core.clj
@@ -1,6 +1,11 @@
-(ns hw6.core)
+(ns hw6.core
+  (:require [hw6.parser :as p])
+  (:import (java.io BufferedReader InputStreamReader))
+  (:gen-class))
 
 (defn -main
   "Run program from standard input and command-line args."
   [& args]
-  (println "Hello, world."))
+  (let [lines (line-seq (BufferedReader. (InputStreamReader. System/in)))]
+    (println (p/parse lines))))
+
---
+++ src/hw6/parser.clj
@@ -0,0 +1,118 @@
+(ns hw6.parser
+  (:require [clojure.contrib.string :as str]))
+
+;;; All data represented as maps:
+;;; - vertex is :x :y :z :dx :dy :dz (location + normal)
+;;; - material is :ambient={:r :g :b} :diffuse={:r :g :b}
+;;;     :specular={:r :g :b :p}
+;;; - settings is :diffuse? :specular? :shadows? :mirror-limit :ambient
+;;; - camera is :i => :pose=vertex
+;;; - light is :type=#{:point :directional} :i :I + :pose=vertex
+;;; - object is :type=#{:sphere :plane :triangle} :material=material + more
+;;;   - :plane also has :i + :pose=vertex
+;;;   - :triangle also has :i :j :k + :v0 :v1 :v2 (all vert/normals)
+;;;   - :sphere also has :i => :center=[x y z] :radius=float
+
+;;; A scene is a map of:
+;;; - :vertices = vector, where vertex ID = index
+;;; - :camera = camera
+;;; - :settings = settings
+;;; - :lights = vector of lights
+;;; - :objects = vector of objects
+;;; - :last-material = material
+
+;;;; Line by line
+
+(def empty-scene
+  {:vertices []
+   :camera {:pose {:x 0 :y 0 :z 0 :dx 0 :dy 0 :dz -1}}
+   :settings {:diffuse? true
+              :specular? true
+              :shadows? true
+              :mirror-max 1
+              :ambient-intensity 1.0}
+   :lights []
+   :objects []
+   :last-material {:ambient {:r 0.2 :g 0.2 :b 0.2}
+                   :diffuse {:r 1.0 :g 1.0 :b 1.0}
+                   :specular {:r 1.0 :g 1.0 :b 1.0 :p 64}}})
+
+(defn parse-float [x] (Float/parseFloat x))
+(defn parse-int [x] (Integer/parseInt x 10))
+
+(defmulti parse-line
+  "Fold the given space-split line into the scene. (Return updated scene.)"
+  (fn [scene code & args] code))
+(defmethod parse-line :default [scene unknown & _]
+  (when-not (or (#{"ts" "tx" "rx" "hx" "ux" "ix" "so" "eo" "io" "rv"} unknown)
+                (= (.substring unknown 0 2) "##"))
+    (throw (Exception. (str "Could not parse line starting with " unknown))))
+  scene)
+;; vertices
+(defmethod parse-line "vv" [scene _ & args]
+  (let [[x y z dx dy dz] (map parse-float args)]
+    (update-in scene [:vertices]
+               conj {:x x :y y :z z :dx dx :dy dy :dz dz})))
+;; materials
+(defmethod parse-line "am" [scene _ & args]
+  (let [[r g b] (map parse-float args)]
+    (assoc-in scene [:last-material :ambient]
+              {:r r :g g :b b})))
+(defmethod parse-line "dm" [scene _ & args]
+  (let [[r g b] (map parse-float args)]
+    (assoc-in scene [:last-material :diffuse]
+              {:r r :g g :b b})))
+(defmethod parse-line "sm" [scene _ r g b p]
+  (let [[r g b] (map parse-float [r g b])
+        p (parse-int p)]
+    (assoc-in scene [:last-material :specular]
+              {:r r :g g :b b :p p})))
+;; objects
+(defmethod parse-line "ss" [scene _ i]
+  (update-in scene [:objects] conj
+             {:type :sphere
+              :i (parse-int i)
+              :material (:last-material scene)}))
+(defmethod parse-line "ps" [scene _ i]
+  (update-in scene [:objects] conj
+             {:type :plane
+              :i (parse-int i)
+              :material (:last-material scene)}))
+;; settings
+(defmethod parse-line "se" [scene _ d s a m I]
+  (assoc-in scene [:settings]
+            {:diffuse? (= d "d")
+             :specular? (= s "s")
+             :shadows? (= a "a")
+             :mirror-limit (parse-int m)
+             :ambient (parse-float I)}))
+;; lights
+(defmethod parse-line "pl" [scene _ i I]
+  (update-in scene [:lights]
+             conj {:type :point :i (parse-int i) :I (parse-float I)}))
+(defmethod parse-line "dl" [scene _ i I]
+  (update-in scene [:lights]
+             conj {:type :directional :i (parse-int i) :I (parse-float I)}))
+;; camera
+(defmethod parse-line "cc" [scene _ i]
+  (assoc-in scene [:camera] {:i (parse-int i)}))
+
+;;;; Fill-in
+
+(defn expand
+  "Fill in vertex values by index, etc."
+  [scene]
+  scene ; TODO
+  )
+
+;;;; Main loop
+
+(defn parse
+  "Parse a sequence of lines into a world"
+  [lines]
+  (let [lines (->> lines
+                   (filter (complement str/blank?) ,,,)
+                   (map (partial str/split #"\s+") ,,,)
+                   (filter (complement nil?) ,,,))]
+    (expand (reduce (partial apply parse-line) empty-scene lines))))
+
