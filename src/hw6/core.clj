af1b7037a9f27eab14c2ee4a2e447dd8218125b4
Tim McCormack <cortex@brainonfire.net>
2011-04-07 17:51:10 -0400

    * Add ability to read (multiple) named files on the command line. * Read from standard input only if no named files.

--- src/hw6/core.clj
+++ src/hw6/core.clj
@@ -1,11 +1,48 @@
 (ns hw6.core
   (:require [hw6.parser :as p])
-  (:import (java.io BufferedReader InputStreamReader))
+  (:import (java.io BufferedReader StringReader InputStreamReader))
+  (:require [clojure.contrib.string :as str])
   (:gen-class))
+
+
+(defn fail
+  "Fail with a message."
+  [msg]
+  (binding [*out* *err*]
+    (println "Failed:" msg))
+  (System/exit 1))
+
+(defn read-arguments
+  "Read command-line arguments into a settings map:
+-f * :in = vector of filenames"
+  [args]
+  (loop [settings {:in []}
+         args args]
+    (if (empty? args)
+      settings
+      (let [flag (first args)
+            remain (rest args)]
+        (condp = flag
+            "-f" (do
+                   (when (empty? remain)
+                     (fail "Expected filename after -f"))
+                   (recur (update-in settings [:in]
+                                     conj (first remain))
+                          (rest remain)))
+            (fail (str "Unknown argument: " flag)))))))
 
 (defn -main
   "Run program from standard input and command-line args."
   [& args]
-  (let [lines (line-seq (BufferedReader. (InputStreamReader. System/in)))]
-    (println (p/parse lines))))
+  (let [settings (read-arguments args)
+        reader (if (seq (:in settings))
+                 (StringReader. (str/join \newline (map slurp (:in settings))))
+                 (InputStreamReader. System/in))]
+    (let [lines (line-seq (BufferedReader. reader))
+          scene (p/parse lines)]
+      (println (format "Loaded %d objects and %d light sources."
+                       (count (:objects scene))
+                       (count (:lights scene))))
+      (println "Scene parsed as:" scene) ;XXX
+      )))
 
