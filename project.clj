8146dc07449d028359493cd73a590fa5dd8fe25f
Tim McCormack <cortex@brainonfire.net>
2012-03-11 00:30:16 -0500

    Version compatibility ranges

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject org.timmc/CS4300-hw6 "0.1.0"
   :description "Tim McCormack HW6 submission for CS 4300, Spring 2011"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [incanter/incanter-core "1.2.3"]]
-  :main org.timmc.cs4300-hw6.core
-  ;:warn-on-reflection true
-  :uberjar-name "timmcHW6-standalone.jar")
+  :dependencies [[org.clojure/clojure "1.3.0"] ;; 1.2.0 through 1.4.0
+                 [incanter/incanter-core "1.3.0"]]
+  :main org.timmc.cs4300-hw6.core)
